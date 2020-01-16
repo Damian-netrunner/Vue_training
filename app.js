dcf116d48029d312cfa7208d8f064aae35d64596
Perumuch <sp_dimon@mail.ru>
2020-01-16 16:42:31 +0800

    Вывод списка

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
+// function car (name, model, owner, phone, image, year) {
+//    return {
+//        name, model, owner, phone, image, year
+//    }
+// }
+
+const car = (name, model, owner, phone, image, year) => ({name, model, owner, phone, image, year}) // сокращенное написание функции
+
+
+
+const cars = [
+    car('Honda', 'Civic', 'Kavinski', '+7123123123', 'Images/Civic.jpg', 2015)
+//    { name: 'Honda', model: 'Civic', owner: 'Kavikski', year: 2015, phone: '+7123123123', image:'Images/Civic.jpg' }
+]
+
+
+
+
 new Vue ({
-    
+    el:'#app',
+    data: {
+cars: cars
+
+    }
 })
