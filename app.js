5c89a8bbb5edb34fc34113a1339712b3018102bf
Perumuch <sp_dimon@mail.ru>
2020-01-17 11:57:22 +0800

    Обработка событий click.
    передача данных в разметку
    Создание функций с данными

--- app.js
+++ app.js
@@ -6,20 +6,23 @@
 
 const car = (name, model, owner, phone, image, year) => ({name, model, owner, phone, image, year}) // сокращенное написание функции
 
-
-
 const cars = [
-    car('Honda', 'Civic', 'Kavinski', '+7123123123', 'Images/Civic.jpg', 2015)
-//    { name: 'Honda', model: 'Civic', owner: 'Kavikski', year: 2015, phone: '+7123123123', image:'Images/Civic.jpg' }
+    car('Honda', 'Civic', 'Kavinski', '+7123123123', 'Images/Civic.jpg', 2015),
+    car('Mitsubishi', 'Lancer', 'Keks', '+7956178913', 'Images/Civic.jpg', 2013),
+    car('Dodge', 'GT', 'Alex', '+753144521', 'Images/Civic.jpg', 2016)
 ]
-
-
 
 
 new Vue ({
     el:'#app',
     data: {
-cars: cars
-
+cars: cars,
+car: cars [0]
+    },
+methods: {
+    selectCar: function(){
+        console.log('click')
     }
+}    
 })
+
