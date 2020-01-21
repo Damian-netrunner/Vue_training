71090056cd1bb0f743f6959459864ff6cef76d86
Perumuch <sp_dimon@mail.ru>
2020-01-21 11:27:36 +0800

    Предварительное завершение
    Логи работают, добавлена анимация телефона

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ const car = (name, model, owner, phone, image, year) => ({
     phone,
     image,
     year
-}) // сокращенное написание функции
+}); // сокращенное написание функции
+const log = (text, type, date = new Date()) => ({text, type, date})
 
 const cars = [
     car('Honda', 'Civic', 'Kavinski', '+7123123123', 'Images/Civic.jpg', 2015),
@@ -23,30 +24,44 @@ const cars = [
 new Vue({
     el: '#app',
     data: {
+        logs: [],
         cars: cars,
         car: cars[0],
         selectedCarIndex: 0,
         phoneVisibility: false,
-        search: ''
+        search: '',
+        modalVisibility: false
     },
     methods: {
         selectCar: function (index) {
 
             this.car = cars[index] // Выбор тачки из списка по нажатию
             this.selectedCarIndex = index // логика для выбора из списка
+        },
+        newOrder(){
+            this.modalVisibility = false
+            this.logs.push(log(`Success Order: ${this.car.name} - ${this.car.model}`, 'ok'))
+        },
+        cancelOrder (){
+            this.modalVisibility = false
+            this.logs.push(log(`Cancelled Order: ${this.car.name} - ${this.car.model}`, 'cnl'))
         }
     },
     computed: {
         PhoneBtnText() {
             return this.phoneVisibility ? 'Hide Phone' : 'Show phone'
         },
-        filteredCars (){
-            var self = this
-            const filtered = this.cars.filter(function(car){
-                return car.name.indexOf(self.search) > -1
+        filteredCars() {
+            return this.cars.filter(car => {
+                return car.name.indexOf(this.search) > -1 || car.model.indexOf(this.search) > -1
             })
-            return filtered
+
         }
     },
+    filters: {
+        date(value){
+            return value.toLocaleString()
+        }
+    }
 
 })
